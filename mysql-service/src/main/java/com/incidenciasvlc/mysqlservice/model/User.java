c650b9b461e859bf0be0fcce247b4a02d1dbfef6
Will <wilfredopaizpaz@gmail.com>
2024-02-12 20:24:01 +0100

    mysql-service created

---
+++ mysql-service/src/main/java/com/incidenciasvlc/mysqlservice/model/User.java
@@ -0,0 +1,33 @@
+package com.incidenciasvlc.mysqlservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "role_id", nullable = false)
+    private int roleId;
+}
---
+++ mysql-service/src/test/java/com/incidenciasvlc/mysqlservice/MySqlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incidenciasvlc.mysqlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
